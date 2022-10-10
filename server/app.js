3520ccd2eea96d4e3bb08e256dad6105610258dd
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2022-10-10 15:44:03 -0500

    Mongo Works

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 // 'Import' the Express module instead of http
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 // Initialize the Express application
 const app = express();
 
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
   // Create the headers for response by default 200
