c427c1c292b2a89b8f986916a8379bb441b09f62
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2022-10-12 20:25:39 -0500

    app.js updates2

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const comments = require("./routers/comments");
+const comments = require("./routers/comment");
 
 dotenv.config();
 // Initialize the Express application
@@ -54,7 +54,7 @@ app.get("/echo/:input", (request, response) => {
   response.status(418).json({ echo: message });
 });
 
-app.use("/comments", comments);
+app.use("/comment", comments);
 
 const PORT = process.env.PORT || 4040; // we use || to provide a default value
 // Tell the Express app to start listening
